fbd4fb1ea64d1b4d83a27f6fc2ac5cde147a02ed
EuSouWill <williamnunesregis@icloud.com>
2024-02-26 20:45:28 -0300

    mudando logo

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParaser = require("body-parser");
 const connection = require('./database/database');
 const Pergunta = require('./database/pergunta');
+const Resposta = require('./database/Resposta');
+
 //Database
 
 connection
@@ -25,7 +27,9 @@ app.locals.partials = true;
 
 //Rotas
 app.get("/", (req, res) => {
-    Pergunta.findAll({raw: true}).then(perguntas =>{
+    Pergunta.findAll({raw: true,order:[
+        ['id','DESC']
+    ]}).then(perguntas =>{
         res.render("index",{
             perguntas: perguntas
         });
@@ -50,6 +54,43 @@ app.post("/salvarpergunta",(req,res)=>{
      });
 });
 
+app.get('/pergunta/:id',(req,res)=>{
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta=>{
+        if(pergunta != undefined){ //pergunta encontrada
+            
+            Resposta.findAll({
+                where:{perguntaId: pergunta.id},
+                order: [['id','DESC']]
+            }).then(respostas =>{
+                res.render("pergunta",{
+                    pergunta:pergunta,
+                    respostas: respostas
+                })
+            })
+            
+           
+
+
+        }else{                    // pergunta nao encontrada
+            res.redirect("/")
+        }
+    });
+});
+
+app.post("/responder",(req,res)=>{
+    var corpo = req.body.corpo;
+    var  perguntaId = req.body.pergunta; 
+    Resposta.create({
+       corpo: corpo,
+       perguntaId: perguntaId,
+    }).then(()=>{
+        res.redirect("/pergunta/"+perguntaId)
+    })
+
+});
 
 app.listen(8080,()=>{
    console.log("Servidor rodando na porta 8080") 
